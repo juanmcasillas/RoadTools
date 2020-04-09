d4db7cffd8d31d1508b3aba4d7a31268b4808a25
Juan M. Casillas <juanm.casillas@gmail.com>
2020-04-09 20:28:09 +0200

    Added support for some face nearing (work regular) and fixed the edge subdivider (works great). Also some test on camera (bad placement of the keyframes) and test about decimate with default parameters (ok)

--- roadtools/bl_flatten.py
+++ roadtools/bl_flatten.py
@@ -22,13 +22,15 @@ class BL_FLATTEN:
             self.vertex = None
             self.edges = []
             self.distance = sys.maxsize
+            self.point = None
 
         def __repr__(self):
-            return "<Nearest face:%s vertex:%s edges:%s distance:%5.8f pos: %s>" % (
+            return "<Nearest face:%s vertex:%s edges:%s distance:%5.8f point:%s pos: %s>" % (
                 self.face, 
                 self.vertex, 
                 self.edges,
                 self.distance, 
+                self.point,
                 self.pos
             )
 
@@ -790,7 +792,202 @@ class BL_FLATTEN:
         #return(("ERROR", "Can't found terrain below the curve"))
         return(("INFO", "Done"))    
 
+
+    # move the points near the curve
+    ### new
+    def move_points(plane, terrain):
+      
+        DEBUG = False
+        LIMIT = None
+        MIN_FACE_DIST = 10
+
+        BL_DEBUG.clear_marks()
+
+        # build a table with the point and the face_idx, to speed up the thing
+        # store the vertex only one time. use raycast to the Z down to check the
+        # points, instead closest.
+        # use side points, and center
+
+        obj_s=  bpy.data.objects[plane]
+        mesh_s = obj_s.data
+        obj_t=  bpy.data.objects[terrain]
+
+        BL_DEBUG.clear_marks()
+
+        bm = bmesh.new()
+        bm.from_mesh(obj_t.data)
+        bm.verts.ensure_lookup_table()
+        bm.edges.ensure_lookup_table()
+        bm.faces.ensure_lookup_table()
+
+        # here, I can flatten or move the thing
+        #for f in  bm.faces:
+        #    if f.select:
+        #        for v in f.verts:
+        #            bm.verts[v.index].co[2] += 5
+
+        verts = []
+        mesh_vertex = []
+        for v in mesh_s.vertices:
+            verts.append(v.index)
+            mesh_vertex.append(v)
+
+        current_faces = [f.index for f in bm.faces if f.select]
+
+        LIMIT = None
+        pc = 0
+        idx = 0
+        MESH_VERTEX_LEN = int(len(mesh_vertex)/2)-1
+
+        # select faces & edges to subdivide
+        process_point = {}
+
+        for pc in range(MESH_VERTEX_LEN):
+            
+            # iterate about the number of quads
+            
+            edges = []
+            if pc == 0:
+                e_1 = ( mesh_vertex[0], mesh_vertex[1] ) # right
+                e_2 = ( mesh_vertex[2], mesh_vertex[3] ) # left
+                idx = 1
+            elif pc == 1:
+                e_1 = ( mesh_vertex[1], mesh_vertex[4] )  # right     
+                e_2 = ( mesh_vertex[3], mesh_vertex[5] )  # left edge
+                idx += 3
+            else:
+                e_1 = ( mesh_vertex[idx], mesh_vertex[idx+2] )       # right 
+                e_2 = ( mesh_vertex[idx+1], mesh_vertex[idx+3] )     # left edge
+                idx += 2
+
+                
+            # 0 is the right side
+            # 1 is the left side 
+            edges = [ e_1, e_2 ]  
+
+            #for e in edges:
+            #    BL_DEBUG.set_mark( obj_t.matrix_world @ e[0].co.xyz, kind='CUBE', scale=0.2 )
+            #    BL_DEBUG.set_mark( obj_t.matrix_world @ e[1].co.xyz, kind='SPHERE', scale=0.2 )
+
+            # calculate the nearest vertex for each point, get the minimum,
+            # and build a face using it
+            
+
+
+            for edge_idx in range(len(edges)):
+                edge =edges[edge_idx]       
+                # for vertex0, vertex1. Pass the "left/right position"
+                near_data = [ BL_FLATTEN.nearest(edge_idx), BL_FLATTEN.nearest(edge_idx) ] 
+                bm.faces.ensure_lookup_table()
+                for i in range(len(edge)):
+                    # was sorround faces
+                    for face_idx in current_faces: 
+                        face = bm.faces[face_idx]
+
+                        # get only faces for our "side"
+                        # note that I use the Plane, not the projected ones.
+
+                        A = obj_s.matrix_world @ edge[0].co
+                        B = obj_s.matrix_world @ edge[1].co
+                        C = obj_t.matrix_world @ face.calc_center_median()
+                        normal_vec = Vector(B-A).cross(Vector(C-A))
+                        normal_rl = normal_vec.z
+                            
+                        if near_data[i].pos == 'LEFT' and normal_rl < 0:
+                            #print("badside left: normal: ", normal_rl, near_data[i].pos, idx) 
+                            continue
+                        if near_data[i].pos == 'RIGHT' and normal_rl > 0:
+                            #print("badside right: normal: ", normal_rl, near_data[i].pos, idx) 
+                            continue
+                        
+                        #print("normal: ", normal_rl, near_data[i].pos, idx, face_idx) 
+
+                        # again, plane vs mesh
+                        for facevertex in face.verts:
+                            vpw = obj_s.matrix_world @ edge[i].co
+                            vtw = obj_t.matrix_world @ facevertex.co                     
+                            dist = (vpw.xyz - vtw.xyz).length
+
+                            if dist < near_data[i].distance:
+                                near_data[i].face = face_idx
+                                near_data[i].vertex = facevertex.index
+                                near_data[i].distance = dist
+                                near_data[i].point = edge[i].index
+                                # calculate the normal (first case, the we do the scond)
+
+                    
+                # select what vertex is near     
+                near = 0 
+                if near_data[1].distance < near_data[0].distance:
+                    near = 1
+                
+                # now, build the array to work with. Store the points in the plane,
+                # data about face,vertex and distance.
+               
+                process_point[near_data[i].point] =  (near_data[near].face, near_data[near].vertex, near_data[near].distance)
+ 
+                
+            #if idx >= 8: break
+            if LIMIT and pc >= LIMIT:
+                break
+
+        # I get all the "nearest" vertex
+        # look in current_faces faces all the edges, and 
+        bm.verts.ensure_lookup_table()
+        bm.faces.ensure_lookup_table()
+
+
+
+        for point_idx in process_point.keys():
+            
+            face_idx, vertex_idx, distance = process_point[point_idx]
+            #
+            # calculate how much I have to move the face, to put it "near the point"
+            # "far" are moved less than "near". use a vector to move the face.
+            #
+
+            for v in bm.faces[face_idx].verts:
+                vpw = obj_s.matrix_world @ mesh_s.vertices[point_idx].co
+                vtw = obj_t.matrix_world @ v.co
+                dist = (vpw.xy - vtw.xy).length
+                height = vpw.z - vtw.z
+
+                # some tunning in this parameters
+                # too high and you break the geometry
+                
+                delta = dist - (dist *0.8)
+                deltah = height*0.1
+
+                direction = delta * (vpw.xy - vtw.xy)
+                direction.resize_3d()
+                direction = delta * direction.normalized()
+                direction += Vector((0,0,deltah))
+                local_dir = obj_t.matrix_world.inverted() @ direction
+
+                print(distance, dist, delta, deltah, local_dir, direction)
+                bmesh.ops.translate(bm, verts=[v], vec = direction)
+
+        ##############################################################
+        #
+        # End
+        # update the meshes
+        # free the bmesh
+        # 
+        ##############################################################  
+
+        bm.calc_loop_triangles()
+        bm.to_mesh(obj_t.data)
+        obj_t.data.update()  
+        bm.free()
+  
+        #return(("ERROR", "Can't found terrain below the curve"))
+        return(("INFO", "Done"))        
+
+
+# better do subdivide, then move
+
 #BL_FLATTEN.extend_terrain('Plane','Terrain')
 #BL_FLATTEN.subdivide_nearest_faces('Plane','Terrain')
+#BL_FLATTEN.move_points('Plane','Terrain')
 #BL_FLATTEN.add_geometry('Plane','Terrain')
 #BL_FLATTEN.fill_holes('Terrain')
